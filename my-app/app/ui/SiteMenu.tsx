2d2db5a3ebb7ef9f42a207e9c85afe52915159e0
yonychg <yonychg@gmail.com>
2025-11-29 13:09:59 +0000

    logo

--- my-app/app/ui/SiteMenu.tsx
+++ my-app/app/ui/SiteMenu.tsx
@@ -9,10 +9,10 @@ interface SidebarProps{
 
 const SiteMenu: FC<SidebarProps> = ({isOpen , onClose}) => {
   return (
-    <div className={`fixed inset-0 h-screen left-0 z-50 w-full bg-black/50 text-white opacity-90 shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>
+    <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white opacity-90 shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>
       <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6'>
         <div className='flex items-center justify-between gap-5'>
-          <Logo className='text-white '></Logo>
+          <Logo className='text-white' spanDesign="group-hover:text-white"></Logo>
           <button onClick={onClose} className="hover:text-shop_light_green hoverEffect">
             <X/>
           </button>
