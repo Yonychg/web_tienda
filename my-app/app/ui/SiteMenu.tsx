1d37d23217f745f6853be069a18d4db441d87296
yonychg <yonychg@gmail.com>
2025-11-30 03:59:20 +0000

    hederlateral

--- my-app/app/ui/SiteMenu.tsx
+++ my-app/app/ui/SiteMenu.tsx
@@ -1,6 +1,9 @@
 import React, { FC } from 'react'
 import Logo from './Logo';
 import { X } from 'lucide-react';
+import { headerData } from '@/Constante/Data';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface SidebarProps{
     isOpen:boolean;
@@ -8,6 +11,7 @@ interface SidebarProps{
 }
 
 const SiteMenu: FC<SidebarProps> = ({isOpen , onClose}) => {
+  const pathname = usePathname();
   return (
     <div className={`fixed inset-y-0 h-screen left-0 z-50 w-full bg-black/50 text-white opacity-90 shadow-xl ${isOpen?"translate-x-0":"-translate-x-full"} hoverEffect`}>
       <div className='min-w-72 max-w-96 bg-black h-screen p-10 border-r border-r-shop_light_green flex flex-col gap-6'>
@@ -16,6 +20,13 @@ const SiteMenu: FC<SidebarProps> = ({isOpen , onClose}) => {
           <button onClick={onClose} className="hover:text-shop_light_green hoverEffect">
             <X/>
           </button>
+        </div>
+        <div className="flex flex-col space-y-3.5 font-semibold tracking-wide ">
+          {headerData?.map((item=>(
+            <Link href={item?.href} key={item?.title} className={`hover:text-shop_light_green hoverEffect ${pathname === item?.href && "text-shop_light_green"}`}>
+            {item?.title}
+            </Link>
+          )))}
         </div>
       </div>
     </div>
